4a420d8ed7c63b66c61dce025dedceb2915129ba
arthur-mf <69751135+arthur-mf@users.noreply.github.com>
2021-10-03 10:31:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
             var pass = $("#pass").val();
             if(name == '' || pass == ''){
                 $("#Required").html('All Feild Are Required').css('color','red');
-            }else if(name == '173-392' && pass == 'R2D2C3'){
+            }else if(name == '173-39' && pass == 'R2D2C3'){
                  $("#form").html('<h4>test</h4><a href="">Back</a>').css('color','green');
              }else{
                  $("#error").html('User Are Not Valid');
