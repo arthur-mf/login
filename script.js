e4efca1b5d292f61864857c1976b40ffb9af6e80
arthur-mf <69751135+arthur-mf@users.noreply.github.com>
2021-10-03 10:38:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+        $("#submit").click(function(){
+            var name= $("#name").val();
+            var pass = $("#pass").val();
+            if(name == '' || pass == ''){
+                $("#Required").html('All Feild Are Required').css('color','red');
+            }else if(name == '173-392' && pass == 'R2D2C3'){
+                 $("#form").html('<h4>test</h4><a href="">Back</a>').css('color','green');
+             }else{
+                 $("#error").html('User Are Not Valid');
+             }
+        });
+    });
